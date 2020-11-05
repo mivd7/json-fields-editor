a59e32fcd23c7728c08e5276e5a522dd00a5c993
MIVD <mi.vandeurzen@gmail.com>
2020-11-05 10:06:24 +0100

    move order list to first screen remove step in between + fix duplicate entries etc.

--- src/components/FieldsConfigurator.vue
+++ src/components/FieldsConfigurator.vue
@@ -3,7 +3,8 @@
     <div class="col-3">
       <div class="field-type-container">
         <h3>Field Types</h3>
-        <draggable class="list-group" :list="availableFields" group="people">
+        <!-- <p>Here are all the fieldtypes available to this flow</p> -->
+        <draggable class="list-group" :list="availableFields" group="people" @change="handleFieldChange">
           <div class="list-group-item" v-for="(field, index) in availableFields" :key="index"
             style="background-color: transparent;">
             {{ field.type }}
@@ -14,12 +15,13 @@
     <div class="col-3">
 
       <h3>Field Groups </h3>
+      <!-- <p>Here you can group fields together</p> -->
 
       <div class="field-group-container">
         <div class="field-group-list" v-for="(group, groupIndex) in groups" :key="groupIndex">
-          
+
           <draggable class="list-group" v-if="group['group' + groupIndex]" :list="group['group' + groupIndex]"
-            group="people" draggable=".list-group-item">
+            group="people" draggable=".list-group-item" :move="checkFieldMove" @change="handleOrderChange('group'+groupIndex)" :sort="checkFieldMove">
             <div slot="header" class="list-group-header" style="background-color: green; color: white;">
               <span>Group {{groupIndex + 1}}
                 <a @click="editFieldContent(group['group' + groupIndex])" 
@@ -27,18 +29,35 @@
               </span>
             </div>
 
-            <div class="list-group-item" v-for="(field, fieldIndex) in group['group' + groupIndex].filter(field => field.type !== 'parent_field_group' + groupIndex)"
+            <div class="list-group-item" v-for="(field, fieldIndex) in group['group' + groupIndex]"
               :key="fieldIndex">
-              
-              <span>{{field.type}} <a @click="editFieldContent(field)" style="color: green; text-decoration: underline; text-align: right;">edit</a></span>
+              <span v-if="field.type.startsWith('parent')">Parent Field <a @click="editFieldContent(field)" style="color: green; text-decoration: underline; text-align: right;">edit</a></span>
+              <span v-else>{{field.type}} <a @click="editFieldContent(field)" style="color: green; text-decoration: underline; text-align: right;">edit</a></span>
             </div>
           </draggable>
         </div>
       </div>
       <button @click="addGroup()">Add group</button>
     </div>
-    
     <div class="col-3">
+      <h3>Field Order</h3>
+      <!-- <p>Change the show order of the fields</p> -->
+      <div class="field-group-container">
+        <div class="field-group-list">
+          <draggable class="list-group" :list="orderedFields" group="people">
+            <div v-for="(value, index) in orderedFields" :key="index">
+
+
+              <div v-if="Array.isArray(value[Object.keys(value)[0]])" class="ordered-fields-item">{{index + 1}}.
+                <span v-for="(val, valIndex) in value[Object.keys(value)[0]]" :key="valIndex">{{val.type}}, </span>
+              </div>
+              <div v-else class="ordered-fields-item">{{index + 1}}. {{value.type}}</div>
+            </div>
+          </draggable>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="col-3">
       <div class="field-type-container">
         <h3>Single Fields</h3>
         <draggable class="list-group" :list="singleFields" group="people">
@@ -48,40 +67,15 @@
           </div>
         </draggable>
       </div>
+    </div> -->
+    <div class="btn-submit-container">
+      <button class="btn-submit" @click="emitResult()">Save configuration</button>
     </div>
+    <!-- <button @click="showChangeOrder = false">Back to grouping</button> -->
 
-    <button class="btn-submit" @click="submit()">Save</button>
-
-  </div>
-
-  <div v-else-if="showChangeOrder" class="row">
-    <div class="col-12">
-      <h3>Field Order</h3>
-
-      <div class="field-group-container">
-        <div class="field-group-list">
-          <draggable class="list-group" :list="result" group="people">
-            <div v-for="(value, index) in result" :key="index">
-
-
-              <div v-if="Array.isArray(value[Object.keys(value)[0]])" class="result-item">{{index + 1}}.
-                  <span v-for="(val, valIndex) in value[Object.keys(value)[0]]" :key="valIndex">{{val.type}}, </span> 
-              </div>
-              <div v-else class="result-item">{{index + 1}}. {{value.type}}</div>
-            </div>
-          </draggable>
-        </div>
-      </div>
-
-    </div>
-    <button v-if="!showChangeOrder" class="btn-submit" @click="submit()">Edit order</button>
-    <div class="btn-submit" v-else>
-      <button @click="showChangeOrder = false">Back to grouping</button>
-      <button @click="submitOrder()">Save configuration</button>
-    </div>
   </div>
   <div v-else-if="showEditField" class="row">
-    <EditFieldForm :field="editFieldItem" v-on:fieldUpdated="showEditField = false"/>
+    <EditFieldForm :field="editFieldItem" v-on:fieldUpdated="showEditField = false" />
   </div>
 </template>
 <script>
@@ -98,9 +92,8 @@
     data() {
       return {
         availableFields: [],
-        singleFields: [],
         groups: [],
-        result: [],
+        orderedFields: [],
         groupAmount: 0,
         showChangeOrder: false,
         showEditField: false,
@@ -119,21 +112,44 @@
         this.groups.push(newGroup)
         this.groupAmount++
       },
-      submit: function () {
-        if(this.availableFields.length > 0) {
-          //add remaining field types as single field
-          this.availableFields.map(fieldType => this.singleFields.push(fieldType))
-          this.availableFields = [];
+      handleOrderChange: function (parentFieldType) {
+        // const doubleEntry = this.filterDuplicates(this.groups, parentFieldType);
+        
+        const groupExists = this.orderedFields.findIndex(field => Object.keys(field)[0] === parentFieldType) !== -1;
+        const groupNumber = Number(parentFieldType.replace(/\D/g, ""));
+        console.log(groupNumber)
+        if (!groupExists) {
+          const result = this.orderedFields.concat(this.groups)
+          const noDuplicates = this.filterDuplicates(result, groupNumber - 1)
+          this.orderedFields = noDuplicates;
+          console.log('orderedFields after no duplicates filter', this.orderedFields)
         }
-        this.result = this.groups.concat(this.singleFields)
-        console.log(this.result)
-        this.showChangeOrder = true;
+        
       },
-      submitOrder: function () {
-        this.$emit('configuratorResult', this.result);
+      emitResult: function () {
+        this.$emit('configuratorResult', this.orderedFields);
       },
-      editFieldContent: function(item) {
-        if(!Array.isArray(item)) { 
+      checkFieldMove: function(e) {
+        if(e.draggedContext.element.type.startsWith('parent_field') || e.draggedContext.futureIndex === 0) {
+          return false;
+        }
+
+      },
+      handleFieldChange: function(e) {
+        if(e.removed) {
+          this.orderedFields = this.orderedFields.filter(field => field.type !== e.removed.element.type);
+        }
+      },
+      filterDuplicates(fieldsArray, prevGroupNum) {
+        const filter = fieldsArray.filter(el => Object.keys(el)[0] === 'group' + prevGroupNum);
+        if(filter.length > 1) {
+          const duplicateIndex = fieldsArray.findIndex(el => Object.keys(el)[0] ==='group' + prevGroupNum);
+          fieldsArray.splice(duplicateIndex, 1);
+        }
+        return fieldsArray;
+      },
+      editFieldContent: function (item) {
+        if (!Array.isArray(item)) {
           this.editFieldItem = item;
           this.showEditField = true;
         } else {
@@ -143,14 +159,16 @@
       },
     },
     created() {
-      if(this.result.length === 0) {
+      if (this.orderedFields.length === 0) {
         dummyFields.map(fieldType => this.availableFields.push({
-          type: fieldType, 
+          type: fieldType,
           header: '',
           description: '',
         }))
+        this.orderedFields = [...this.availableFields];
       }
-    }
+    },
+
   };
 </script>
 <style>
@@ -204,7 +222,7 @@
     border: 1px solid rgba(0, 0, 0, .125);
   }
 
-  .btn-submit {
+  .btn-submit-container {
     display: block;
     margin: 10px auto
   }
@@ -222,8 +240,8 @@
     padding: 0 15px 15px 15px;
     margin: 0 25px 25px 25px;
   }
-  
-  .result-item {
+
+  .ordered-fields-item {
     margin-top: 5px;
     border: 1px solid grey;
   }
